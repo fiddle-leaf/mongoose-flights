a3ff48e7be4c18d38acd289739a7a5601f55a9f9
fiddle.leaf <shanonps@icloud.com>
2023-08-28 21:16:31 -0400

    Routes created for app

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,22 +2,86 @@
  *  *       *   Import Our Dependencies    *       *      *  *
  **     *       *       *       *       *       *      *    **
  */
- const express = require("express");
- const Snack = require("../models/Snack");
- 
- /**     *       *       *       *       *       *      *    **
-  *  *       *       *     Create Router     *       *        *
-  **     *       *       *       *       *       *      *    **
-  */
- const router = express.Router();
- 
- /**     *       *       *       *       *       *       *      **
-  *  *       *       *      *    Routes      *       *       *   *
-  * Remember INDUCES order: index, new, delete, update, edit, show
-  **     *       *       *       *       *       *       *      **
-  */
- 
- // INDEX
- 
- module.exports = router;
- 
+const express = require("express");
+const Flight = require("../models/Flight");
+
+/**     *       *       *       *       *       *      *    **
+ *  *       *       *     Create Router     *       *        *
+ **     *       *       *       *       *       *      *    **
+ */
+const router = express.Router();
+
+/**     *       *       *       *       *       *       *      **
+ *  *       *       *      *    Routes      *       *       *   *
+ * Remember INDUCES order: index, new, delete, update, edit, show
+ **     *       *       *       *       *       *       *      **
+ */
+
+// INDEX
+router.get("/", (req, res) => {
+  Flight.find({})
+    .then((foundFlights) => {
+      res.render("flights/index", {
+        flights: foundFlights,
+      });
+    })
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// NEW
+router.get("/:id", (req, res) => {
+  res.render("flights/new");
+});
+
+// DELETE
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Flight.deleteOne({ _id: id })
+    .then((data) => res.redirect("/flights"))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// UPDATE
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Flight.updateOne({ _id: id }, req.body, { new: true })
+    .then((data) => res.redirect("/flights"))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// CREATE
+router.post("/", (req, res) => {
+  Flight.create(req.body)
+    .then((data) => res.redirect("/flights"))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// EDIT
+router.get("/:id/edit", (req, res) => {
+  const id = req.params.id;
+
+  Flight.findOne({ _id: id })
+    .then((foundFlight) => {
+      res.render("flights/edit", {
+        flight: foundFlight,
+      });
+    })
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// SHOW
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Flight.findOne({ _id: id })
+    .then((foundFlight) => {
+      res.render("flight/show", {
+        flight: foundFlight,
+      });
+    })
+    .catch((error) => res.status(400).json({ error }));
+});
+
+module.exports = router;
--- models/seed.js
+++ models/seed.js
@@ -1,19 +1,40 @@
-const flights = [
-  {
-    airline: "American",
-    flightNo: 61357432,
-    departs: "2024-01-13T04:20",
-  },
-  {
-    airline: "Southwest",
-    flightNo: 21352963,
-    departs: "2023-11-02T18:15",
-  },
-  {
-    airline: "United",
-    flightNo: 50989452,
-    departs: "2024-07-18T11:17",
-  },
-];
+const mongoose = require("./connection");
+const Flight = require("./Flight");
 
-module.exports = flights;
+const db = mongoose.connection;
+
+db.on("open", () => {
+  // array of starter flights
+  const flights = [
+    {
+      airline: "American",
+      flightNo: 61357432,
+      departs: "2024-01-13T04:20",
+    },
+    {
+      airline: "Southwest",
+      flightNo: 21352963,
+      departs: "2023-11-02T18:15",
+    },
+    {
+      airline: "United",
+      flightNo: 50989452,
+      departs: "2024-07-18T11:17",
+    },
+  ];
+
+  // Delete all flights
+  Flight.deleteMany({})
+    .then((data) => {
+      Flight.create(flights)
+        .then((data) => {
+          db.close();
+        })
+        .catch((error) => {
+          db.close();
+        });
+    })
+    .catch((error) => {
+      db.close();
+    });
+});
