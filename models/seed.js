2d05853f84611e75fb5ab6b2f98bc5517d0bb421
fiddle.leaf <shanonps@icloud.com>
2023-08-28 20:50:37 -0400

    Created seed module to render

--- models/seed.js
+++ models/seed.js
@@ -1,1 +1,19 @@
- 
+const flights = [
+  {
+    airline: "American",
+    flightNo: 61357432,
+    departs: "2024-01-13T04:20",
+  },
+  {
+    airline: "Southwest",
+    flightNo: 21352963,
+    departs: "2023-11-02T18:15",
+  },
+  {
+    airline: "United",
+    flightNo: 50989452,
+    departs: "2024-07-18T11:17",
+  },
+];
+
+module.exports = flights;
